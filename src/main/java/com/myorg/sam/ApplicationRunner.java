26188b89c8fcb1a211dda6a08305a56b41f42338
s0s01ts <siddesh.shivakumar@walmartlabs.com>
2019-05-31 20:12:16 +0530

    first commit

---
+++ src/main/java/com/myorg/sam/ApplicationRunner.java
@@ -0,0 +1,29 @@
+package com.myorg.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class ApplicationRunner extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+    @Bean
+    public Docket courseApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.myorg.sam.controller")).build();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApplicationRunner.class);
+    }
+}
---
+++ src/main/java/com/myorg/sam/controller/MyController.java
@@ -0,0 +1,33 @@
+package com.myorg.sam.controller;
+
+import com.myorg.sam.entity.MyEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Set;
+
+@Api(value = "Course information", description = "Api related to course information")
+@Controller
+@RequestMapping("/sam")
+public class MyController {
+
+    @ApiOperation(value = "view all courses")
+    @RequestMapping(value = "/view", method = RequestMethod.GET)
+    public ResponseEntity<MyEntity> get() {
+        MyEntity m = new MyEntity();
+        m.setAge(10);
+        m.setName("test");
+        m.setId(2);
+        return ResponseEntity.ok(m);
+    }
+
+}
